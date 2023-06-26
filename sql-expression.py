418ee87cda1dba0920168a924c90e56fc17837a8
choyoon88 <azureluster@gmail.com>
2023-06-26 18:58:00 +0000

    sql orm

--- sql-expression.py
+++ sql-expression.py
@@ -47,7 +47,8 @@ with db.connect() as connection:
     # [artist_table.c.Name])
 
     # Query 3 - select only 'Queen' from the "Artist" table
-    select_query = artist_table.select().where(artist_table.c.Name == "Queen")
+    select_query = artist_table.select().where(
+        artist_table.c.Name == "Queen")
 
     # Query 4 - select only by 'ArtistId' #51 from the "Artist" table
     # select_query = artist_table.select().where(artist_table.c.ArtistId == 51)
